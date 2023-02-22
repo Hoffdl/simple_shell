9efb993260f54acfca9ca3dc7684958ac99ff25a
Henry <ikegwwuonuhenry25@gmail.com>
2023-02-22 01:31:57 -0800

    Simple Shell

--- shell_loop.c
+++ shell_loop.c
@@ -50,7 +50,7 @@ int hsh(info_t *info, char **av)
  * Return: -1 if builtin not found,
  * 			0 if builtin executed successfully,
  * 			1 if builtin found but not successful,
- * 			2 if builtin signals exit()
+ * 			-2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
